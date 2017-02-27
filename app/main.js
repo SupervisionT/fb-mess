babcd8115b0a93793897f5e0953b207262666c3b
besart <besartshyti@gmail.com>
2017-02-27 23:26:21 +0200

    add logs

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ var router = require('./router.js');
 
 module.exports = (req, res) => {
   var pathUrl = `${req.method} ${req.url}`;
+  console.log('pathUrl',pathUrl);
   try {
     router[pathUrl](req,res);
   } catch (error) {
